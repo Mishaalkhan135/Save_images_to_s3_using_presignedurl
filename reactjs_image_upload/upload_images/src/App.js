fd147661b36b02e53d3aa531fb85a4569e4a1e5b
Mishaalkhan135 <80804969+Mishaalkhan135@users.noreply.github.com>
2022-10-12 00:57:34 +0500

    uploading images to s3 using http api

---
+++ reactjs_image_upload/upload_images/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Uploader from "../component/Uploader";
+
+function App() {
+	return (
+		<div className='App'>
+			<Uploader />
+		</div>
+	);
+}
+
+export default App;
